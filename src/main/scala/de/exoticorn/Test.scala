76378708863c7f14c91760cf4ad9f0a5be903628
Dennis Ranke <mail@dennisranke.de>
2011-12-29 00:02:46 +0100

    trying out nio buffers without org.lwjgl.BufferUtils

--- src/main/scala/de/exoticorn/Test.scala
+++ src/main/scala/de/exoticorn/Test.scala
@@ -38,16 +38,24 @@ object Test extends App {
   val program = linkProgram(vertexShader, fragmentShader)
 
   checkError()
-  
-  val vertexBuffer = BufferUtils.createFloatBuffer(6)
+
+  val byteBuffer = java.nio.ByteBuffer.allocateDirect(6*4)
+  byteBuffer.order(java.nio.ByteOrder.nativeOrder())
+  val vertexBuffer = byteBuffer.asFloatBuffer()
   vertexBuffer.put(Array(-1.0f, -1.0f, 1.0f, -1.0f, 0.0f, 1.0f))
   vertexBuffer.rewind()
 
   val positionAttribute = glGetAttribLocation(program, "position")
+
+  var t = 0.0
   
   while(!Display.isCloseRequested()) {
     glClearColor(1, 0, 1, 1)
     glClear(GL_COLOR_BUFFER_BIT)
+
+    byteBuffer.putFloat(16, math.sin(t).asInstanceOf[Float])
+//    vertexBuffer.put(0, math.sin(t).asInstanceOf[Float])
+    t += math.Pi / 60
 
     glUseProgram(program)
     glVertexAttribPointer(positionAttribute, 2, false, 8, vertexBuffer)
